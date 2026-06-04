f390fd05f8141003d91fa26b7960c6ccf8dcad43
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2026-06-04 08:23:47 +0545

    feat: add RegisterView view class for accounts

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from accounts.models import CustomUser
+from accounts.serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
