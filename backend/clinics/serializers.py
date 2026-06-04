67584e86154efb8acbb7d32e131e213391145474
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2026-06-04 08:32:13 +0545

    feat: define serializer classes for clinics app

---
+++ backend/clinics/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from clinics.models import Clinic, Doctor, Schedule
+
+class ClinicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clinic
+        fields = '__all__'
+
+class DoctorSerialzier(serializers.ModelSerializer):
+    schedules = ScheduleSerialzer(many=True, read_only=True)
+    clinic_name = serializers.CharField(source='clinic.name', read_only=True)
+    
+    class Meta:
+        model = Doctor
+        fields = ['id', 'first_name', 'last_name', 'specialty', 'is_active', 'clinic_name', 'schedules']
+
+class ScheduleSerialzer(serializers.ModelSerializer):
+    day_name = serializers.CharField(source='get_day_of_week_display', read_only=True)
+
+    class Meta:
+        model = Schedule
+        fields = ['id', 'day_of_week', 'day_name', 'start_time', 'end_time', 'max_patients', 'avg_consultation_minutes']
+
