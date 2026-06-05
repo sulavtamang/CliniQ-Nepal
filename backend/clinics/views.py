63db9d36ff8161ce19a200365c2eefe927d8c6b8
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2026-06-05 09:22:52 +0545

    feat: define view for clinics app

--- backend/clinics/views.py
+++ backend/clinics/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from clinics.models import Doctor
+from clinics.serializers import DoctorSerialzier
 
-# Create your views here.
+#generic view for listing active doctors publicly
+class DoctorListView(generics.ListAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerialzier
+    permission_classes = (AllowAny,)
