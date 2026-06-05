6b43e53361c4c83594827a06e3c75670d1bd6d82
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2026-06-05 09:21:27 +0545

    refactor: refactor serializer classes of clinics app

--- backend/clinics/serializers.py
+++ backend/clinics/serializers.py
@@ -6,14 +6,6 @@ class ClinicSerializer(serializers.ModelSerializer):
         model = Clinic
         fields = '__all__'
 
-class DoctorSerialzier(serializers.ModelSerializer):
-    schedules = ScheduleSerialzer(many=True, read_only=True)
-    clinic_name = serializers.CharField(source='clinic.name', read_only=True)
-    
-    class Meta:
-        model = Doctor
-        fields = ['id', 'first_name', 'last_name', 'specialty', 'is_active', 'clinic_name', 'schedules']
-
 class ScheduleSerialzer(serializers.ModelSerializer):
     day_name = serializers.CharField(source='get_day_of_week_display', read_only=True)
 
@@ -21,3 +13,10 @@ class ScheduleSerialzer(serializers.ModelSerializer):
         model = Schedule
         fields = ['id', 'day_of_week', 'day_name', 'start_time', 'end_time', 'max_patients', 'avg_consultation_minutes']
 
+class DoctorSerialzier(serializers.ModelSerializer):
+    schedules = ScheduleSerialzer(many=True, read_only=True)
+    clinic_name = serializers.CharField(source='clinic.name', read_only=True)
+    
+    class Meta:
+        model = Doctor
+        fields = ['id', 'first_name', 'last_name', 'specialty', 'is_active', 'clinic_name', 'schedules']
