d869ab9fc57740540cbcd04b3149cfb1b3d80ef2
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2026-06-07 08:06:46 +0545

    fix: fix typo of the import

--- backend/clinics/views.py
+++ backend/clinics/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from clinics.models import Doctor
-from clinics.serializers import DoctorSerialzier
+from clinics.serializers import DoctorSerializer
 
 #generic view for listing active doctors publicly
 class DoctorListView(generics.ListAPIView):
     queryset = Doctor.objects.all()
-    serializer_class = DoctorSerialzier
+    serializer_class = DoctorSerializer
     permission_classes = (AllowAny,)
